1c11b1def9f12c63d5bc5de167ae7cdac2fda0bd
yellowdollar <yellowdollar55@gmail.com>
2024-08-28 06:15:39 +0500

    test

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 @app.exception_handler(404)
 async def not_found_handler(request: Request, exc: Exception):
-    return RedirectResponse(url='/')
+    return RedirectResponse(url='/admin_panel')
 
 @app.get('/')
 def admin_panel(request: Request):
